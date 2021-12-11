53eb6937b5898b230a3acd6b55eb46eb3355c807
mogy-h <rapter.mg@gmail.com>
2021-12-11 17:27:56 +0900

    functionの書き方を統一

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../components/Layout";
 import Image from "next/image";
 
-const Contact = () => {
+export default function Contact() {
   return (
     <Layout title="Contact Page">
       <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
@@ -74,6 +74,4 @@ const Contact = () => {
       </div>
     </Layout>
   );
-};
-
-export default Contact;
+}
