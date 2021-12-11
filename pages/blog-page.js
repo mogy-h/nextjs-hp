fcf278986520e41cbcce9dba54380555149d0547
mogy-h <rapter.mg@gmail.com>
2021-12-11 16:05:14 +0900

    blog-pageのダイナミックルーティングの実装

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,8 +1,20 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { getAllPostsData } from "../lib/posts";
 
-const Blog = () => {
-  return <Layout title="Blog Page">blog page</Layout>;
+ export default function Blog({ posts }) {
+  return (
+    <Layout title="Blog Page">
+      <ul className="m-10">{posts && posts.map((post) => <Post key={post.id} post={post} />)}</ul>
+    </Layout>
+  );
 };
 
-export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
