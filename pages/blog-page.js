ea33fc1d7f94461b27d5a76282665c48ff4a7be5
mogy-h <rapter.mg@gmail.com>
2021-12-11 13:03:15 +0900

    ヘッダー、フッターの作成

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import Layout from "../components/Layout";
 
-export const Blog = () => {
-  return (
-    <div>
-      blog page
-    </div>
-  )
-}
+const Blog = () => {
+  return <Layout title="Blog Page">blog page</Layout>;
+};
+
+export default Blog;
